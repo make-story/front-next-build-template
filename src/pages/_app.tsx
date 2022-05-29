deec8c8c6846d7d28492e71060061be3cde0d6a3
sung-min <yu9221@gmail.com>
2022-05-29 19:22:12 +0900

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,6 +15,9 @@ import { RootState } from 'src/rootReducer';
 
 // https://nextjs.org/docs/basic-features/typescript#custom-app
 const App: NextComponentType<AppContext, AppInitialProps, AppProps> = ({ Component, pageProps }) => {
+  // <Component /> 타입 에러가 발생하는 경우, package.json 등 버전이 맞지 않다는 것이다.
+  // yarn.lock 파일 필요(의존성 종속관계 성공한 버전)
+  // 또는 package.json 의존성 하나하나 각각 설치 시도
   return <Component {...pageProps} />;
 };
 
