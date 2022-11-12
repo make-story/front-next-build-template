f08929740eb50c213bea419ac8cc61ea5e960b61
sung-min <yu9221@gmail.com>
2022-11-12 14:27:27 +0900

    update

--- src/project/components/test/index.tsx
+++ src/project/components/test/index.tsx
@@ -1,8 +1,26 @@
 /**
  * 테스트
  */
-import React, { useEffect } from 'react';
+ import React, { useState, useCallback, useEffect, useReducer, useRef, Suspense } from 'react';
+ import { ReactNode } from 'react';
+ import Router, { useRouter } from 'next/router';
+ import { useDispatch, useSelector } from 'react-redux';
+ import dynamic from 'next/dynamic';
 
-export default function Test() {
-  return <section className=''>TEST</section>;
-}
+const DynamicTest = dynamic(() => import('../event/index'), {
+  //ssr: false,
+  loading: () => <p>Loading...</p>,
+  //suspense: true,
+});
+
+interface IProps {}
+
+const Test = React.forwardRef<any, any>((props: IProps, ref) => {
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const [details, setDetails] = useState<boolean>(false);
+
+  return <>{!details ? <button onClick={() => setDetails(true)}>CLICK ME</button> : <DynamicTest />}</>;
+});
+
+export default Test;
