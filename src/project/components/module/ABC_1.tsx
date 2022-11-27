9c23b662bf02d57bbb7f1cd83a1936e117577ed4
Sung-min Yu <yu9221@gmail.com>
2022-11-27 20:08:37 +0900

    update

--- src/project/components/module/ABC_1.tsx
+++ src/project/components/module/ABC_1.tsx
@@ -5,7 +5,14 @@ const ABC_1 = () => {
     // 데이터 호출
   }, []);
 
-  return <>ABC_1</>;
+  return (
+    <>
+      ABC_1
+      <div>
+        <a href={'//naver.com'}>네이버</a>
+      </div>
+    </>
+  );
 };
 
 export default ABC_1;
